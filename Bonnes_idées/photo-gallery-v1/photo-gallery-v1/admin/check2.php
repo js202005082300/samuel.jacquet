4c156bc8c92445fb9a54e7e6f2819a2428c90ecc
js <js201910271836@outlook.com>
2021-01-15 01:24:51 +0100

    git pull https://github.com/js202005082300/samuel.jacquet.github.io.git

--- Bonnes_idées/photo-gallery-v1/photo-gallery-v1/admin/check2.php
+++
@@ -1,7 +0,0 @@
-<?Php
-if((isset($_SESSION['userid']) and strlen($_SESSION['userid']) > 4) and $_SESSION['userid'] == 'admin'){
-}else{
-echo 'Please <a href=login.php>login</a>';
-exit;
-}
-?>
--- Bonnes_idées/photo-gallery-v1/photo-gallery-v1/menu.php
+++
@@ -1,5 +0,0 @@
-<?Php
-
-echo " <a href=gallery.php>Add Gallery</a> . <a href=upload.php>Add Image</a> .  <a href=index.php>Home</a> .  <br>";
-
-?>
